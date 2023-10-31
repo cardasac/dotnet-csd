6aa25b52b8f7d8a67c41664be63d5c77ce9048ea
Alexandru Constantin Cardas <calexc@alviralex.com>
2023-10-31 13:40:37 +0000

    Change load tests

--- load_tests/main.js
+++ load_tests/main.js
@@ -5,12 +5,12 @@ import http from 'k6/http'
 export const options = {
   stages: [
     { duration: '1m', target: 20 },
-    { duration: '3m', target: 20 },
+    { duration: '1m', target: 20 },
     { duration: '1m', target: 0 },
   ],
   thresholds: {
-    http_req_failed: ['rate<0.01'], // http errors should be less than 2%
-    http_req_duration: ['p(95)<1000'], // 95% requests should be below 2s
+    http_req_failed: ['rate<0.02'], // http errors should be less than 2%
+    http_req_duration: ['p(95)<2000'], // 95% requests should be below 2s
   },
   ext: {
     loadimpact: {
@@ -22,6 +22,6 @@ export const options = {
 }
 
 export default function main() {
-  http.get('https://alex-csd.azurewebsites.net/')
+  http.get('https://alex-csd-staging.azurewebsites.net/')
   sleep(1)
 }
