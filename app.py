a04ecd544b7853293f96ce0243132b56bf1097e1
Alexandru Constantin Cardas <calexc@alviralex.com>
2023-12-03 14:31:10 +0000

    Fix sonar issues

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from asgiref.wsgi import WsgiToAsgi
+from flask_wtf.csrf import CSRFProtect
 
 application = Flask(__name__, template_folder="src/templates")
+csrf = CSRFProtect()
+csrf.init_app(application)
 @application.route("/")
 def hello_world():
     return render_template("hello.html")
