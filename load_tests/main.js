54a1e998e156728977bbfe4fa47c87c1784e967b
Alexandru Constantin Cardas <calexc@alviralex.com>
2023-12-08 14:40:05 +0000

    Fix load tests

--- load_tests/main.js
+++ load_tests/main.js
@@ -7,7 +7,7 @@ export const options = {
     { duration: '1m', target: 20 }
   ],
   thresholds: {
-    http_req_failed: ['count=0'], // http errors should be less than 1%
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(99.9)<20'], // 99.9% requests should be below 20ms
   },
   ext: {
