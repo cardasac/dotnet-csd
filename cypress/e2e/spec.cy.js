0351e67b64310ee2f629da65910d352b6ab9a2f9
Alexandru Constantin Cardas <calexc@alviralex.com>
2023-12-08 18:58:54 +0000

    Add test videos

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("form submit", () => {
+  beforeEach(() => {
+    cy.visit(Cypress.env("APP_URL"));
+  });
+
+  it("submit with builtin submit", () => {
+    cy.get("#systolic").type(120);
+    cy.get("#diastolic").type(80);
+    cy.get("form").submit();
+    cy.contains("Ideal").should("be.visible");
+  });
+
+  it("submit with button click", () => {
+    cy.get("#systolic").type(120);
+    cy.get("#diastolic").type(80);
+    cy.get("[type='submit']").click();
+  });
+});
