da67d092aeb254d6f5ba9136b50acd9e72851612
Alexandru Constantin Cardas <calexc@alviralex.com>
2023-12-08 01:43:37 +0000

    Tidy up error checking

--- src/forms.py
+++ src/forms.py
@@ -1,17 +1,17 @@
 """Main forms for the server."""
 from flask_wtf import FlaskForm
 from wtforms import IntegerField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import InputRequired, NumberRange
 
 
 class MyForm(FlaskForm):
     """Form for blood pressure."""
 
     systolic = IntegerField(
-        "systolic",
-        validators=[DataRequired(), NumberRange(min=70, max=190)],
+        "Systolic",
+        validators=[InputRequired(), NumberRange(min=70, max=190)],
     )
     diastolic = IntegerField(
-        "diastolic",
-        validators=[DataRequired(), NumberRange(min=40, max=80)],
+        "Diastolic",
+        validators=[InputRequired(), NumberRange(min=40, max=100)],
     )
